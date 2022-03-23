2d55adc099c299eb99ff7cdaa403c0b0fa62d5b3
nttminh-dev <mnguyen@bhcc.edu>
2022-03-23 16:41:46 -0400

    feat: 🎸 create new component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 export { default as Navbar } from "./Navbar";
 export { default as People } from "./People";
 export { default as Planets } from "./Planets";
+export { default as Planet } from './Planet'
+export { default as Person } from './Person'
