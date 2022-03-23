bf5b3276fe2de3e05decb8f74dd0c2c62212e9d8
nttminh-dev <mnguyen@bhcc.edu>
2022-03-23 16:40:24 -0400

    feat: 🎸 add react-query

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
 import { useState } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { Navbar, People, Planets } from './components';
 
+const queryClient = new QueryClient()
 
 function App() {
   const [page, setPage] = useState('planets');
 
   return (
-    <div className="App">
-      <h1>Star Wars Info</h1>
-      <Navbar setPage={setPage} />
-      <div className="content">
-        {page === 'planets' ? <Planets /> : <People />}
+    <QueryClientProvider client={queryClient}>
+
+      <div className="App">
+        <h1>Star Wars Info</h1>
+        <Navbar setPage={setPage} />
+        <div className="content">
+          {page === 'planets' ? <Planets /> : <People />}
+        </div>
       </div>
-    </div>
+    </QueryClientProvider>
   );
 }
 
