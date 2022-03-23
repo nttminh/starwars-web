8795a7ca6714fb8b072a05f804e522a5e128abc7
nttminh-dev <mnguyen@bhcc.edu>
2022-03-23 15:45:36 -0400

    feat: 🎸 add navbar

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import { Navbar, People, Planets } from './components';
+
+
+function App() {
+  const [page, setPage] = useState('planets');
+
+  return (
+    <div className="App">
+      <h1>Star Wars Info</h1>
+      <Navbar />
+      <div className="content">
+        {page === 'planets' ? <Planets /> : <People />}
+      </div>
+    </div>
+  );
+}
+
+export default App;
